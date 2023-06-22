a11ae7fca436d4a219c106766e68f89ab3e14377
loicmaes <production@maesloic.fr>
2023-06-22 13:01:14 +0200

    :sparkles: Add permissions to bypass preventions

--- src/main/java/fr/maesloic/netherrp/netherhub/spigot/events/PreventionListener.java
+++ src/main/java/fr/maesloic/netherrp/netherhub/spigot/events/PreventionListener.java
@@ -33,13 +33,14 @@ public class PreventionListener implements Listener {
     // DAMAGES
     @EventHandler
     public final void damage(final @NotNull EntityDamageEvent event) {
+        final Entity entity                       = event.getEntity();
         final EntityDamageEvent.DamageCause cause = event.getCause();
 
         event.setCancelled(switch (cause) {
-            case BLOCK_EXPLOSION, ENTITY_EXPLOSION -> !(boolean) Objects.requireNonNull(Settings.EXPLOSION_DAMAGES.value());
-            case FALL -> !(boolean) Objects.requireNonNull(Settings.FALL_DAMAGES.value());
-            case FIRE, FIRE_TICK -> !(boolean) Objects.requireNonNull(Settings.FIRE_DAMAGES.value());
-            case DROWNING -> !(boolean) Objects.requireNonNull(Settings.DROWNING_DAMAGES.value());
+            case BLOCK_EXPLOSION, ENTITY_EXPLOSION -> this.isPrevented(entity, "damages.explosion", !(boolean) Objects.requireNonNull(Settings.EXPLOSION_DAMAGES.value()));
+            case FALL -> this.isPrevented(entity, "damages.fall", !(boolean) Objects.requireNonNull(Settings.FALL_DAMAGES.value()));
+            case FIRE, FIRE_TICK -> this.isPrevented(entity, "damages.fire", !(boolean) Objects.requireNonNull(Settings.FIRE_DAMAGES.value()));
+            case DROWNING -> this.isPrevented(entity, "damages.drowning", !(boolean) Objects.requireNonNull(Settings.DROWNING_DAMAGES.value()));
             default -> false;
         });
     }
@@ -49,48 +50,51 @@ public class PreventionListener implements Listener {
         final Entity victim  = event.getEntity();
         final Entity damager = event.getDamager();
 
-        if (victim instanceof Player && damager instanceof Player && !(boolean) Objects.requireNonNull(Settings.PVP.value())) event.setCancelled(true);
-        if (damager instanceof Player && !(victim instanceof Player) && !(boolean) Objects.requireNonNull(Settings.PVE.value())) event.setCancelled(true);
+        event.setCancelled(
+                this.isPrevented(damager, "damages.pvp", victim instanceof Player && damager instanceof Player && !(boolean) Objects.requireNonNull(Settings.PVP.value())) ||
+                this.isPrevented(damager, "damages.pve", damager instanceof Player && !(victim instanceof Player) && !(boolean) Objects.requireNonNull(Settings.PVE.value()))
+        );
     }
 
     // Y LIMIT
     @EventHandler
     public final void move(final @NotNull PlayerMoveEvent event) {
         final Player player = event.getPlayer();
-
-        if (!Settings.VERTICAL_LIMIT.isSet() || event.getTo().getY() >= (int) Objects.requireNonNull(Settings.VERTICAL_LIMIT.value())) return;
-        player.teleport(this.plugin.spawn());
+        if (this.isPrevented(player, "world.v_limit", Settings.VERTICAL_LIMIT.isSet() && event.getTo().getY() < (int) Objects.requireNonNull(Settings.VERTICAL_LIMIT.value()))) player.teleport(this.plugin.spawn());
     }
 
     // PLAYER
     @EventHandler
     public final void foodLoss(final @NotNull FoodLevelChangeEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.FOOD_LEVEL_CHANGE.value()));
+        event.setCancelled(this.isPrevented(event.getEntity(), "player.food_loss", !(boolean) Objects.requireNonNull(Settings.FOOD_LEVEL_CHANGE.value())));
     }
 
     @EventHandler
     public final void bubble(final @NotNull EntityAirChangeEvent event) {
-        event.setCancelled(event.getEntity() instanceof Player && !(boolean) Objects.requireNonNull(Settings.AIR_LEVEL_CHANGE.value()));
+        event.setCancelled(this.isPrevented(event.getEntity(), "player.bubble_loss", event.getEntity() instanceof Player && !(boolean) Objects.requireNonNull(Settings.AIR_LEVEL_CHANGE.value())));
     }
 
     // BUILD
     @EventHandler
     public final void placeBlock(final @NotNull BlockPlaceEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.PLACE_BLOCK.value()));
+        event.setCancelled(this.isPrevented(event.getPlayer(), "build.place", !(boolean) Objects.requireNonNull(Settings.PLACE_BLOCK.value())));
     }
 
     @EventHandler
     public final void placeBreak(final @NotNull BlockBreakEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.BREAK_BLOCK.value()));
+        event.setCancelled(this.isPrevented(event.getPlayer(), "build.break", !(boolean) Objects.requireNonNull(Settings.BREAK_BLOCK.value())));
     }
 
     // INTERACTIONS
     // > World
+    @Deprecated
     @EventHandler
     public final void playerInteract(final @NotNull PlayerInteractEvent event) {
         final ItemStack item = event.getItem();
         final Player player  = event.getPlayer();
         final Action action  = event.getAction();
+        if (event.isCancelled()) return;
+
         if (Objects.nonNull(item) && item.getType().equals(Material.PLAYER_HEAD)) {
             event.setCancelled(true);
 
@@ -109,33 +113,38 @@ public class PreventionListener implements Listener {
         final Block block = event.getClickedBlock();
         assert Objects.nonNull(block);
 
-        if (!(boolean) Objects.requireNonNull(Settings.OPEN_DOOR.value()) && block.getType().name().contains("_DOOR")) event.setCancelled(true);
-        if (!(boolean) Objects.requireNonNull(Settings.OPEN_TRAP_DOOR.value()) && block.getType().name().contains("_TRAPDOOR")) event.setCancelled(true);
-        if (!(boolean) Objects.requireNonNull(Settings.OPEN_FENCE_GATE.value()) && block.getType().name().contains("_FENCE_GATE")) event.setCancelled(true);
-        if (!(boolean) Objects.requireNonNull(Settings.PRESS_BUTTON.value()) && block.getType().name().contains("_BUTTON")) event.setCancelled(true);
-        if (!(boolean) Objects.requireNonNull(Settings.TOGGLE_LEVER.value()) && block.getType().equals(Material.LEVER)) event.setCancelled(true);
-        if (!(boolean) Objects.requireNonNull(Settings.ACTIVATE_PRESSURE_PLATE.value()) && block.getType().name().contains("_PRESSURE_PLATE")) event.setCancelled(true);
-
+        event.setCancelled(
+                this.isPrevented(player, "interactions.world.doors", !(boolean) Objects.requireNonNull(Settings.OPEN_DOOR.value()) && block.getType().name().contains("_DOOR")) ||
+                this.isPrevented(player, "interactions.world.trapdoors", !(boolean) Objects.requireNonNull(Settings.OPEN_TRAP_DOOR.value()) && block.getType().name().contains("_TRAPDOOR")) ||
+                this.isPrevented(player, "interactions.world.fence_gates", !(boolean) Objects.requireNonNull(Settings.OPEN_FENCE_GATE.value()) && block.getType().name().contains("_FENCE_GATE")) ||
+                this.isPrevented(player, "interactions.world.buttons", !(boolean) Objects.requireNonNull(Settings.PRESS_BUTTON.value()) && block.getType().name().contains("_BUTTON")) ||
+                this.isPrevented(player, "interactions.world.levers", !(boolean) Objects.requireNonNull(Settings.TOGGLE_LEVER.value()) && block.getType().equals(Material.LEVER)) ||
+                this.isPrevented(player, "interactions.world.pressure_plates", !(boolean) Objects.requireNonNull(Settings.ACTIVATE_PRESSURE_PLATE.value()) && block.getType().name().contains("_PRESSURE_PLATE"))
+        );
     }
 
     // > Inventory
     @EventHandler
     public final void inventoryClick(final @NotNull InventoryClickEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.INVENTORY_CLICK.value()));
+        event.setCancelled(this.isPrevented(event.getWhoClicked(), "interactions.inventory.click", !(boolean) Objects.requireNonNull(Settings.INVENTORY_CLICK.value())));
     }
 
     @EventHandler
     public final void itemDrop(final @NotNull PlayerDropItemEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.DROP_ITEM.value()));
+        event.setCancelled(this.isPrevented(event.getPlayer(), "interactions.inventory.drop", !(boolean) Objects.requireNonNull(Settings.DROP_ITEM.value())));
     }
 
     @EventHandler
     public final void itemPickup(final @NotNull EntityPickupItemEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.PICKUP_ITEM.value()));
+        event.setCancelled(this.isPrevented(event.getEntity(), "interactions.inventory.pickup", !(boolean) Objects.requireNonNull(Settings.PICKUP_ITEM.value())));
     }
 
     @EventHandler
     public final void itemSwap(final @NotNull PlayerSwapHandItemsEvent event) {
-        event.setCancelled(!(boolean) Objects.requireNonNull(Settings.SWAP_HAND.value()));
+        event.setCancelled(this.isPrevented(event.getPlayer(), "interactions.inventory.swap", !(boolean) Objects.requireNonNull(Settings.SWAP_HAND.value())));
+    }
+
+    private Boolean isPrevented(final @NotNull Entity player, final @NotNull String permission, final Boolean condition) {
+        return !player.hasPermission("netherhub.bypass.%s".formatted(permission)) && condition;
     }
 }
